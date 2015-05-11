f75d00c38b40e0fb7a8f3818cc493d357bd31136
1R151-1 <ryan.feargal@gmail.com>
2015-05-11 10:16:12 +0100

    Update SQL_R.r

--- SQL_R.r
+++ SQL_R.r
@@ -1,20 +1,5 @@
 
 # ---------------------------------------------------------------------------------------------
-# Functions for working with SQL in R
-# ---------------------------------------------------------------------------------------------
-
-# Read in a SQL file from the wroking directory
-# e.g. read.sql("sql/test.sql")
-
-	read.sql <- function(filename, silent = TRUE) {
-		q <- readLines(filename, warn = !silent)
-		q <- q[!grepl(pattern = "^\\s*--", x = q)] # remove full-line comments
-		q <- sub(pattern = "--.*", replacement="", x = q) # remove midline comments
-		q <- paste(q, collapse = " ")
-		return(q)
-	}
-	
-# ---------------------------------------------------------------------------------------------
 #'
 #' sqlQry(sqlString, execute = F, connection = conn,  view = F, csv = NULL, output.dir = "output/)
 #'
@@ -26,8 +11,8 @@
 #' Default behaviour is to not actually run but return a vector containing SQL statements.
 #'
 #' If you execute SQL against the database, there are options to
-#'  • View() the results of the queries immediatly
-#'  • Save the results of the queries (to .csv)
+#'  . View() the results of the queries immediatly
+#'  . Save the results of the queries (to .csv)
 #'
 #' If a single statement is run a dataframe will be returned
 #' If multiple statements are run a list will be returned
@@ -40,11 +25,15 @@
 # ---------------------------------------------------------------------------------------------
     sqlQry <- function(sqlString,
                        execute = FALSE,
-                       connection = conn,
                        view = FALSE,
+                       connection = conn,                       
                        csv = NULL,
                        output.dir = "output/"){
         sqlString <- str_trim(sqlString)
+
+        require(dplyr)
+        require(stringr)
+        require(RODBC)
 
         if (str_sub(sqlString, -4) == ".sql"){ # if input string is a file then get it
             sqlString <- read.sql(sqlString)
@@ -57,6 +46,13 @@
             for ( i in 1:length(sqlString)){
                 qry <- sqlString[i]
                 data.out[[i]] <- sqlQuery( connection, qry)
+                if(is.character(data.out[[i]])){
+                   data.out[[i]] <-  paste(data.out[[i]][1], collapse = "\n") # Only return the error message not the SQL
+                }
+                if (is.data.frame(data.out[[i]])){
+                    data.out[[i]] <- tbl_df(data.out[[i]]) # Wrapper for better terminal printing of dataframes
+                    names(data.out[[i]]) <- tolower(names(data.out[[i]]))
+                }
             }
 
             if (length(data.out) == 1){
@@ -67,14 +63,16 @@
                 write.csv(data.out, csv)
             } else if  ( length(csv) != 0 & length(csv) != length(data.out)){
                 warning("Data will not be saved to CSV as number of outputs differs from the number of file names provided", call. = F)
-            } else if ( length(csv) == length(data.out)){
+            } else if ( length(csv) >0 & length(csv) == length(data.out)){
                 for(i in 1:length(csv)){
                     if(csv[i] != ""){write.csv(data.out[[i]], paste(output.dir, csv[i], str_replace_all(Sys.time(), ":", "-"), ".csv", sep = ""))}
                 }
             }
-            if(view){
+            if(view & class(data.out)[1] == "tbl_df"){
+                View(data.out)
+            } else if (view & class(data.out)[1] == "list"){
                 for(i in 1:length(data.out)) View(data.out[[i]], paste("Query", i))
-                }
+            }
             return (data.out)
         } else {
             return (sqlString)
