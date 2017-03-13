be7f90a41eef02989ffc1a4f06ceaaab27d82f24
fdryan <fdryan@users.noreply.github.com>
2017-03-13 10:09:01 +0000

    Add significance argument
    Allows function to accept an argument specifying the number of significant numbers to return

--- ggplot2_formatter.r
+++ ggplot2_formatter.r
@@ -15,7 +15,9 @@ require(scales)
 #' numbers where billions, millions or thousands are appropriate.
 #'
 #' @return a character vector the same length as the input vector
-#' @param x a numeric vector to format, smbl a symbol you'd like to prefix your numbers by
+#' @param x a numeric vector to format, 
+#' @smbl a symbol you'd like to prefix your numbers by e.g. "$"
+#' @signif = the number of significant places you want the function to return
 #' @examples
 #' human_numbers(c(1000000 , 1500000, 10000000000))
 #' human_numbers(c(1.200000e+05, -2.154660e+05, 2.387790e+05, 4.343500e+04 ,5.648675e+12), "$")
@@ -23,33 +25,37 @@ require(scales)
 #' ggplot2 + scale_x_continuous(labels = human_numbers)
 #' ggplot2 + scale_x_continuous(labels = human_gbp)
 
-human_numbers <- function(x = NULL, smbl =""){
-  humanity <- function(y){             
-    
+human_numbers <- function(x = NULL, smbl ="", signif = 1){
+  humanity <- function(y){
+
     if (!is.na(y)){
-      
-       b <- round_any(abs(y) / 1e9, 0.1)
-       m <- round_any(abs(y) / 1e6, 0.1)
-       k <- round_any(abs(y) / 1e3, 0.1)
-      
-      if ( y >= 0 ){ 
+        tn <- round(abs(y) / 1e12, signif)
+       b <- round(abs(y) / 1e9, signif)
+       m <- round(abs(y) / 1e6, signif)
+       k <- round(abs(y) / 1e3, signif)
+
+      if ( y >= 0 ){
         y_is_positive <- ""
       } else {
         y_is_positive <- "-"
       }
-      
+
       if ( k < 1 ) {
-        paste0(y_is_positive, smbl, y )
+        paste0( y_is_positive, smbl, round(abs(y), signif ))
         } else if ( m < 1){
         paste0 (y_is_positive, smbl,  k , "k")
       } else if (b < 1){
         paste0 (y_is_positive, smbl, m ,"m")
+      }else if(tn < 1){
+          paste0 (y_is_positive, smbl, b ,"bn")
       } else {
-        paste0 (y_is_positive, smbl,  comma(b), "b")     
+        paste0 (y_is_positive, smbl,  comma(tn), "tn")
       }
+    } else if (is.na(y) | is.null(y)){
+        "-"
     }
   }
-  
+
   sapply(x,humanity)
 }
 
